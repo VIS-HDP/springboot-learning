2df81280c2aec47a5231b5b6e1f94163768c1c2d
gaojianhua <596902726@qq.com>
2017-12-14 14:11:21 +0800

    'test'

--- springboot-hello/src/main/java/com/vis/learn/HelloController.java
+++ springboot-hello/src/main/java/com/vis/learn/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by Administrator on 2017/11/22.
+ * Created by robin on 2017/11/22. 
  */
 @RestController
 public class HelloController {
@@ -14,6 +14,6 @@ public class HelloController {
     @GetMapping("/users")
     public String sayHello(){
         System.out.println(" enter sayHello ...111 ");
-        return "Springboot1233";
+        return "Springboot1233" ;
     }
 }
